07c69be6424d7a3d00848d70b59834c0bdccbab2
younghearts <331912665@qq.com>
2020-10-12 11:32:41 +0800

    获取了外部md数据

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,35 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
-
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+function Home({allPostsData}) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>next demo</p>
-        <p>这是我的一个next.js demo</p>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
-      <Link href="/posts/first-post">first-post</Link>
     </Layout>
   )
 }
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+export default Home
